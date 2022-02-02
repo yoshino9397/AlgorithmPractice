0756ebb9a5d04b524583db0312f9751bdd19501d
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-02-01 22:03:16 -0800

    Merge Two Sorted Lists

--- Blind75/script.js
+++ Blind75/script.js
@@ -10,3 +10,60 @@ const twoSum = function (nums, target) {
   return arr;
 };
 console.log(twoSum([2, 7, 11, 15], 9));
+
+///2,Longest Substring Without Repeating Characters
+const lengthOfLongestSubstring = function (s) {
+  if (s.length < 2) {
+    return s.length;
+  }
+  var map = [];
+  var len = 0;
+  var maxLen = len;
+  var start = 0;
+
+  for (let i = start; i < s.length; i++) {
+    c = s[i];
+    if (map[c] !== undefined && map[c] >= start) {
+      start = map[c] + 1; // start new search with repeated word's last location + 1
+      len = i - start; // real length -> from for example 3 to 5 is 3, so it's 5-3+1 and + 1 happens later
+    }
+    len++; // real length -> from for example 3 to 5 is 3, so it's 5-3+1 and + 1 happens later
+    if (len > maxLen) {
+      maxLen = len;
+    }
+    map[c] = i;
+  }
+  return maxLen;
+};
+console.log(lengthOfLongestSubstring("pwwkew"));
+
+/// 7,Valid Parentheses
+const isValid = function (s) {
+  var stack = [];
+  for (let i = 0; i < s.length; i++) {
+    let c = s[i];
+    if (c == "{" || c == "(" || c == "[") {
+      stack.push(c);
+    } else if (c == "}" || c == ")" || c == "]") {
+      let a = stack.pop();
+      if (
+        !a ||
+        (a == "{" && c !== "}") ||
+        (a == "[" && c !== "]") ||
+        (a == "(" && c !== ")")
+      ) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+console.log(isValid("()["));
+
+/// 8,Merge Two Sorted Lists
+const mergeTwoLists = function (list1, list2) {
+  let arr = [...list1, ...list2];
+  arr.sort((a, b) => a - b);
+  return arr
+};
+console.log(mergeTwoLists([1, 2, 4], [1, 3, 5]));
